9c28942cd8fc4627a9de901eb8a06a368ad2925f
NTrongTinh <trongtinh12b4lh2022@gmail.com>
2024-05-28 10:01:26 +0700

    ChITietPhieuNhapMap

---
+++ test2/Models/Mapping/HhChiTietPhieuNhapMap.cs
@@ -0,0 +1,21 @@
+﻿using test2.Models.Entities;
+
+namespace test2.Models.Mapping
+{
+    public class HhChiTietPhieuNhapMap
+    {
+        public long Id { get; set; }
+
+        public string? IdhangHoa { get; set; }
+
+        public string? IdphieuNhap { get; set; }
+
+        public string? SoLuong { get; set; }
+
+        public string? DonGia { get; set; }
+
+        public virtual HhDmHangHoa? IdhangHoaNavigation { get; set; }
+
+        public virtual HhPhieuNhap IdphieuNhapNavigation { get; set; } = null!;
+    }
+}
